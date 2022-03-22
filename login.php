97b669a6b9c1f9885f62360ed7012e2a3035cc3f
eternalodball <84136173+eternalodball@users.noreply.github.com>
2022-03-22 19:56:59 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+$user=$_POST["username"];
+$pass = $_POST["password"];
+
+if ($user == "admin" && $pass == "admin") {
+    echo "Login was successful!";
+  } else {
+    echo "Have a good night!";
+  }
+
+session_destroy();
+
+
+?>
